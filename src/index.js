01dc8436caeca88021f5b27193865951e60b7838
D. Adams <contact@mianlabs.com>
2018-09-30 20:11:49 -0400

    Add the Bulma framework with SCSS compilation via Webpack

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers';
 const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+require('./scss/main.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -20,7 +21,19 @@ module.exports = {
         use: [
           { loader: 'babel-loader' }
         ]
-      }
-		]
-	}
+      }, 
+      {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'sass-loader'
+        ]
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+  ]
 };
