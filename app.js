23e0b7fa3be9f0870e8cd382f009b4ba7a22de89
D. Adams <contact@mianlabs.com>
2018-10-14 18:02:47 -0400

    Add koa ejs and Hello World with template views

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ global.rootRequire = name => require(`${__dirname}/${name}`);
 
 require('dotenv').config();
 
+const path = require('path');
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
+const render = require('koa-ejs');
 
 const logger = require('koa-logger');
 
@@ -16,8 +19,16 @@ if (process.env.NODE_ENV !== 'production') {
 	app.use(logger());
 }
 
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layouts/index',
+  viewExt: 'ejs',
+  cache: false,
+  debug: process.env.NODE_ENV !== 'production' ? true : false
+});
+
 router.get('/', async ctx => {
-	ctx.body = 'Hello World!';
+	await ctx.render('hello');
 });
 
 app.use(router.routes());
