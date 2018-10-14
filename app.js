4135720cdfb132b5a92e4352d64f6283f854900e
D. Adams <contact@mianlabs.com>
2018-10-14 18:22:32 -0400

    Add bodyparser and method-override

--- app.js
+++ app.js
@@ -19,12 +19,16 @@ render(app, {
 
 const logger = require('koa-logger');
 const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const override = require('koa-override');
 
 if (process.env.NODE_ENV !== 'production') {
 	app.use(logger());
 }
 
 app.use(serve(path.join(__dirname, 'public')));
+app.use(bodyParser());
+app.use(override());
 
 const indexRoutes = rootRequire('routes/index');
 
