58be7c88400aa41c21772b5e635ad237c6749aab
D. Adams <contact@mianlabs.com>
2018-10-14 17:19:11 -0400

    Add dev logger for koa

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 const Koa = require('koa');
 const app = new Koa();
 
+const logger = require('koa-logger');
+
+if (process.env.NODE_ENV !== 'production') {
+	app.use(logger());
+}
+
 app.use(async ctx => {
 	ctx.body = 'Hello World!';
 });
