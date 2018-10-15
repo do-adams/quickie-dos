425635fb4db077272c5695e4b35511ccd2e5c4dc
D. Adams <contact@mianlabs.com>
2018-10-14 21:11:38 -0400

    Add Stimulus and Turbolinks and wire them into the hello view

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+'use strict';
+
+// SCSS
 require('./scss/main.scss');
 
+// Stimulus
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+const application = Application.start();
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+// Turbolinks
+const Turbolinks = require('turbolinks');
+Turbolinks.start();
