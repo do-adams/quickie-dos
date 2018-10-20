50a8f2948bfbc25919a6d592913cd0556ff8c4fe
D. Adams <contact@mianlabs.com>
2018-10-20 12:59:03 -0400

    Add koa session middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const path = require('path');
 
+// MONGOOSE SETUP
+
 const mongoose = require('mongoose');
 
 const dbUrl = process.env.DATABASE_URL || 'mongodb://localhost/quickie_dos';
@@ -15,6 +17,8 @@ mongoose.connect(dbUrl, { useNewUrlParser: true }, function(err) {
 		process.exit(1);
 	}
 });
+
+// INITIAL KOA SETUP
 
 const Koa = require('koa');
 const app = new Koa();
@@ -28,11 +32,14 @@ render(app, {
   debug: process.env.NODE_ENV !== 'production' ? true : false
 });
 
+// KOA MIDDLEWARE SETUP
+
 const logger = require('koa-logger');
 const helmet = require('koa-helmet');
 const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const override = require('koa-override');
+const session = require('koa-session');
 
 if (process.env.NODE_ENV !== 'production') {
 	app.use(logger());
@@ -43,6 +50,24 @@ app.use(serve(path.join(__dirname, 'public')));
 app.use(serve(path.join(__dirname, 'dist')));
 app.use(bodyParser());
 app.use(override());
+
+// KOA SESSION MIDDLEWARE
+app.keys = [process.env.SESSION_KEY || 'secret session development key'];
+
+const CONFIG = {
+  key: 'koa:sess',
+  maxAge: 86400000,
+  autoCommit: true,
+  overwrite: true,
+  httpOnly: true,
+  signed: true,
+  rolling: false,
+  renew: false,
+};
+
+app.use(session(CONFIG, app));
+
+// KOA ROUTES SETUP
 
 const indexRoutes = rootRequire('routes/index');
 
