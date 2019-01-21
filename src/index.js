b7ae01ba5bfae2e34478eb1c909261ee840f32b6
Damián Adams <damian@polygraphmedia.com>
2019-01-20 20:59:29 -0400

    Remove Stimulus and Turbolinks from the project

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@
 
 // SCSS
 import './scss/main.scss';
-
-// Stimulus
-import { Application } from 'stimulus';
-import { definitionsFromContext } from 'stimulus/webpack-helpers';
-
-const application = Application.start();
-const context = require.context('./controllers', true, /\.js$/);
-application.load(definitionsFromContext(context));
-
-// Turbolinks
-const Turbolinks = require('turbolinks');
-Turbolinks.start();
