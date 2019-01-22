43ccb9547bb8f4a0c310e0a9cf65faeff5800eb8
Damián Adams <contact@mianlabs.com>
2019-01-22 19:17:00 -0400

    Move the Webpack modules into one consolidated bundle for ease of use (and to avoid using broken loaders)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
 'use strict';
 
-require('dotenv').config();
-
 const path = require('path');
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
 	},
 	mode: process.env.NODE_ENV || 'development',
@@ -34,7 +28,7 @@ module.exports = {
 				use: [
           'style-loader',
           {
-            loader: 'css-loader',
+            loader: 'css-loader', 
             options: {
               sourceMap: true
             }
@@ -44,14 +38,16 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          'style-loader',
           {
-            loader: 'css-loader', options: {
+            loader: 'css-loader', 
+            options: {
               sourceMap: true
             }
           },  
           {
-            loader: 'sass-loader', options: {
+            loader: 'sass-loader', 
+            options: {
               sourceMap: true
             }
           }]
@@ -63,30 +59,7 @@ module.exports = {
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
       new VueLoaderPlugin()
-    ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.css$/,
-            chunks: 'all',
-            enforce: true
-          }
-        }
-      },
-      minimizer: [
-        new UglifyJsPlugin({
-          cache: true,
-          parallel: true,
-          sourceMap: true
-        }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    }
+    ]
   };
   
