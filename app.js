0b09c15589a86a98291844906448b9b04392439c
Damián Adams <contact@mianlabs.com>
2019-01-26 15:49:50 -0400

    Get a Hello World running with Pug

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
